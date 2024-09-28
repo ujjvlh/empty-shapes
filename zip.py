f8da796f3e0e9037758a718dc9b46b64b4cdcb1c
ujjvlh <ujjwal.raajput@gmail.com>
2024-09-28 14:53:51 +0530

    a

--- zip.py
+++ zip.py
@@ -2,16 +2,16 @@ from pathlib import Path
 import os
 
 names = [
-    ["simplified-water-polygons-split-3857", "simplified_water_polygons"],
-    ["water-polygons-split-3857", "water_polygons"],
-    ["antarctica-icesheet-polygons-3857", "icesheet_polygons"],
-    ["antarctica-icesheet-outlines-3857", "icesheet_outlines"],
-    ["", "ne_110m_admin_0_boundary_lines_land"]
+    ["simplified-water-polygons-split-3857", "simplified_water_polygons", "p"],
+    ["water-polygons-split-3857", "water_polygons", "p"],
+    ["antarctica-icesheet-polygons-3857", "icesheet_polygons", "p"],
+    ["antarctica-icesheet-outlines-3857", "icesheet_outlines", "l"],
+    ["", "ne_110m_admin_0_boundary_lines_land", "l"]
 ]
 
 for name in names:
     path = name[0] if name[0] else name[1]
     Path(path).mkdir(parents=True, exist_ok=True)
     for ext in ['cpg', 'dbf', 'prj', 'shp', 'shx']:
-        os.system(f'cp empty.{ext} {path}/{name[1]}.{ext}')
+        os.system(f'cp empty{"" if name[2]=="p" else "-lines"}.{ext} {path}/{name[1]}.{ext}')
     os.system(f'zip -r{"" if name[0] else " -j"} {path}.zip {path}')
